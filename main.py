5110a87bdb779749a9798a2b1210cbee0142e48b
Tommy Daniels <thomsondaniels@hotmail.com>
2023-07-30 20:15:28 +1000

    username

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from language_stats import GitHubLanguageStats
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Code pollution detector; how many bytes have you written?")
+    parser.add_argument('-u', '--username', default='', help='Your GitHub username')
     parser.add_argument('-t', '--token', default='', help='Your GitHub Pesonal Access Token (PAT)')
     parser.add_argument('-i', '--ignore', nargs='+', default=[], help='An ignore list of repository names')
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    username = 'tomdaniels'
-    stats = GitHubLanguageStats(username, token=args.token, ignore_list=args.ignore)
+    stats = GitHubLanguageStats(username=args.username, token=args.token, ignore_list=args.ignore)
     print(stats.fetch_languages())
 
 if __name__ == "__main__":
