ff045d5031e00dbed7d138ae119baa3065d02700
Tommy Daniels <thomsondaniels@hotmail.com>
2023-07-30 18:36:13 +1000

    consolidated fetch_data fnc

--- language_stats.py
+++ language_stats.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 from halo import Halo
 
 class GitHubLanguageStats:
+    BASE_URL = 'https://api.github.com'
+
     def __init__(self, username, token, ignore_list=None):
         self.username = username
         self.ignore_list = ignore_list or []
@@ -11,18 +13,19 @@ class GitHubLanguageStats:
         self.spinner = Halo(text='Fetching and calculating...', spinner='dots')
         self.headers = {'Authorization': f'token {self.token}'}
 
-    def fetch_repos(self):
-        url = f'https://api.github.com/user/repos?per_page=100'
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        repos = response.json()
-        return [repo['name'] for repo in repos if repo['name'] not in self.ignore_list]
-
-    def fetch_repo_languages(self, repo_name):
-        url = f'https://api.github.com/repos/{self.username}/{repo_name}/languages'
+    def fetch_data(self, url):
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.json()
+
+    def fetch_repos(self):
+        url = f'{self.BASE_URL}/user/repos?per_page=100'
+        repos = self.fetch_data(url)
+        return [repo['name'] for repo in repos if repo['name'] not in self.ignore_list]
+
+    def fetch_repo_languages(self, repo_name):
+        url = f'{self.BASE_URL}/repos/{self.username}/{repo_name}/languages'
+        return self.fetch_data(url)
 
     def aggregate_languages(self, language_dicts):
         totals = defaultdict(int)
