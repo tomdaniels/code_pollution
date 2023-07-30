9bd539eaf6cfcebd60373475a246e779f66c6c9d
Tommy Daniels <thomsondaniels@hotmail.com>
2023-07-30 18:23:04 +1000

    help info screen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from language_stats import GitHubLanguageStats
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Ever wondered how many bytes you have written in each programming language?')
+    parser = argparse.ArgumentParser(description="Code pollution detector; how many bytes have you written?")
     parser.add_argument('-t', '--token', default='', help='Your GitHub Pesonal Access Token (PAT)')
     parser.add_argument('-i', '--ignore', nargs='+', default=[], help='An ignore list of repository names')
     return parser.parse_args()
