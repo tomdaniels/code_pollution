c0db23963101ceee4d4923614e96f01bf60e62e5
Tommy Daniels <thomsondaniels@hotmail.com>
2023-08-01 22:09:56 +1000

    partial application

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import argparse
 
+from functools import partial
 from language_stats import GitHubLanguageStats
 
+description = "Code pollution detector; how many bytes have you written?"
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Code pollution detector; how many bytes have you written?")
-    parser.add_argument('-u', '--username', default='', help='Your GitHub username')
-    parser.add_argument('-t', '--token', default='', help='Your GitHub Pesonal Access Token (PAT)')
-    parser.add_argument('-i', '--ignore', nargs='+', default=[], help='An ignore list of repository names')
+    parser = argparse.ArgumentParser(description)
+    add_argument = partial(parser.add_argument)
+
+    add_argument('-u', '--username', default='', help='Your GitHub username')
+    add_argument('-t', '--token',    default='', help='Your GitHub Pesonal Access Token (PAT)')
+    add_argument('-i', '--ignore',   default=[], help='An ignore list of repository names', nargs='+')
+    
     return parser.parse_args()
 
 def main():
